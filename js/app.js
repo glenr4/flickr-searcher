eec1560f91abae638f5c499e2b12a65826622966
glenr4 <glenrutherford4@gmail.com>
2016-03-19 07:51:43 +0800

    http request working

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 angular.module('flickrApp', ['ngMessages'])
 .controller('flickrCtrl', function($scope, $http){
 
+	
 
+	$scope.submit = function(){
+		var url = "https://api.flickr.com/services/rest";
+		var params = {
+	    method: 'flickr.photos.search',
+	    api_key: 'af1df5d17d123f5acf4da73848a1c8c7',
+	    tags: $scope.searchTag,
+	    format: 'json',
+	    nojsoncallback: 1
+		};
 
-
-
+		$http({
+			url: url,
+			params: params
+		})
+		.then(function(response){
+			console.log(response);
+		});
+	};
 
 
 });
